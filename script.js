a3a8dbe486be86c54818060da37f556a02881e97
ARNAB9669 <arnabbiswas9669@gmail.com>
2025-07-14 14:56:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+/* This is for dawn*/
+
+function showPopup_1() {
+  document.getElementById('popup_1').classList.add('active');
+}
+
+function closePopup_1() {
+  document.getElementById('popup_1').classList.remove('active');
+}
+
+document.getElementById('C_1').addEventListener('click', function () {
+  showPopup_1();
+});
+
+/* This is for break-fast*/
+
+function showPopup_2() {
+  document.getElementById('popup_2').classList.add('active');
+}
+
+function closePopup_2() {
+  document.getElementById('popup_2').classList.remove('active');
+}
+
+document.getElementById('C_2').addEventListener('click', function () {
+  showPopup_2();
+});
+
+/* This is for morning*/
+
+function showPopup_3() {
+  document.getElementById('popup_3').classList.add('active');
+}
+
+function closePopup_3() {
+  document.getElementById('popup_3').classList.remove('active');
+}
+
+document.getElementById('C_3').addEventListener('click', function () {
+  showPopup_3();
+});
+
+/* This is for lunch*/
+
+function showPopup_4() {
+  document.getElementById('popup_4').classList.add('active');
+}
+
+function closePopup_4() {
+  document.getElementById('popup_4').classList.remove('active');
+}
+
+document.getElementById('C_4').addEventListener('click', function () {
+  showPopup_4();
+});
+
+/* This is for tea-break*/
+
+function showPopup_5() {
+  document.getElementById('popup_5').classList.add('active');
+}
+
+function closePopup_5() {
+  document.getElementById('popup_5').classList.remove('active');
+}
+
+document.getElementById('C_5').addEventListener('click', function () {
+  showPopup_5();
+});
+
+/* This is for dinner*/
+
+function showPopup_6() {
+  document.getElementById('popup_6').classList.add('active');
+}
+
+function closePopup_6() {
+  document.getElementById('popup_6').classList.remove('active');
+}
+
+document.getElementById('C_6').addEventListener('click', function () {
+  showPopup_6();
+});
+
+/* This is for bed-time*/
+
+function showPopup_7() {
+  document.getElementById("popup_7").classList.add('active');
+}
+
+function closePopup_7() {
+  document.getElementById("popup_7").classList.remove('active');
+}
+
+document.getElementById('C_7').addEventListener('click', function () {
+  showPopup_7();
+});
